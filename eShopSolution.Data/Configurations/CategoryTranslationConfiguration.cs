dac15c3a71e503f1c8e6f974359d69adff661fb7
LAPTOP-DJTS74VH\admin <phubeo14@gmail.com>
2025-10-19 23:07:41 +0700

    Add Entity, Configuration entities with Fluent API, Complete configuration, Migration database

---
+++ eShopSolution.Data/Configurations/CategoryTranslationConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using eShopSolution.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Configurations
+{
+    public class CategoryTranslationConfiguration : IEntityTypeConfiguration<CategoryTranslation>
+    {
+        public void Configure(EntityTypeBuilder<CategoryTranslation> builder)
+        {
+            builder.ToTable("CategoryTranslations");
+            builder.HasKey(c => c.Id);
+            builder.Property(c => c.Id).UseIdentityColumn();
+            builder.Property(c => c.Name).IsRequired().HasMaxLength(200);
+            builder.Property(c => c.SeoAlias).IsRequired().HasMaxLength(200);
+            builder.Property(c => c.SeoDescription).IsRequired().HasMaxLength(200);
+            builder.Property(c => c.SeoTitle).IsRequired().HasMaxLength(200);
+            builder.Property(c => c.LanguageId).IsUnicode(false).HasMaxLength(5);
+            builder.HasOne(l => l.Language).WithMany(ct => ct.CategoryTranslations).HasForeignKey(ct => ct.LanguageId);
+            builder.HasOne(c => c.Category).WithMany(ct => ct.CategoryTranslations).HasForeignKey(ct => ct.CategoryId);
+        }
+    }
+}

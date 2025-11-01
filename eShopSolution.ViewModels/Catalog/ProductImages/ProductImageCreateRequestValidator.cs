5ba706ba06628d165b0cd096337c91892f20efbc
LAPTOP-DJTS74VH\admin <phubeo14@gmail.com>
2025-11-01 11:03:49 +0700

    Validate create, update request image product

---
+++ eShopSolution.ViewModels/Catalog/ProductImages/ProductImageCreateRequestValidator.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace eShopSolution.ViewModels.Catalog.ProductImages
+{
+    public class ProductImageCreateRequestValidator : AbstractValidator<ProductImageCreateRequest>
+    {
+        public ProductImageCreateRequestValidator()
+        {
+            RuleFor(x => x.Caption)
+                .MaximumLength(200).WithMessage("The caption must not exceed 200 characters."); // exceed equals over
+
+            RuleFor(x => x.IsDefault).NotNull().WithMessage("IsDefault must be specified (true or false).");
+
+            RuleFor(x => x.ImageFile).NotEmpty().WithMessage("Image file is required");
+            RuleFor(x => x.ImageFile)
+                .NotEmpty().WithMessage("Image file is required.")
+                .Must(file => file.Length <= 5 * 1024 * 1024)
+                    .WithMessage("Image file size must not exceed 5MB.")
+                .Must(file => new List<string> { ".jpg", ".jpeg", ".png" }
+                    .Contains(Path.GetExtension(file.FileName).ToLower()))
+                    .WithMessage("Only .jpg, .jpeg, or .png files are allowed.");
+
+            RuleFor(x => x.SortOrder)
+                .GreaterThanOrEqualTo(0).WithMessage("Sort order must be non-negative.");
+        }
+    }
+}
---
+++ eShopSolution.ViewModels/Catalog/ProductImages/ProductImageUpdateRequestValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace eShopSolution.ViewModels.Catalog.ProductImages
+{
+    public class ProductImageUpdateRequestValidator : AbstractValidator<ProductImageUpdateRequest>
+    {
+        public ProductImageUpdateRequestValidator()
+        {
+            RuleFor(x => x.ImageFile)
+                .NotEmpty().WithMessage("Image file is required.")
+                .Must(file => file.Length <= 5 * 1024 * 1024)
+                    .WithMessage("Image file size must not exceed 5MB.")
+                .Must(file => new List<string> { ".jpg", ".jpeg", ".png" }
+                    .Contains(Path.GetExtension(file.FileName).ToLower()))
+                    .WithMessage("Only .jpg, .jpeg, or .png files are allowed.");
+        }
+    }
+}
